55eaa0af933e7d5cf06d57450afe1f39ba901e5f
jauafelipe <jauafelipe345@gmail.com>
2023-01-27 15:55:05 -0300

    project 1 js

---
+++ assets/script.js
@@ -0,0 +1,43 @@
+function relogio() {
+    function getTimeSeconds (segundos) {
+        const data = new Date(segundos * 1000)
+        return data.toLocaleTimeString('Pt-BR', {
+            hour12: false,  
+            timeZone: 'UTC'
+        })
+    
+    }
+    
+    
+    const relogio = document.querySelector('.relogio');
+    let seconds = 0
+    let timer;
+    
+    function startTimer () {
+        timer = setInterval(() => {
+            seconds++
+            relogio.innerHTML = getTimeSeconds(seconds)
+        }, 1000);
+    }
+    
+    document.addEventListener('click', (e) => {
+        const elements = e.target;
+    
+        if(elements.classList.contains('iniciar')) {
+            clearInterval(timer)
+            startTimer()
+        }
+        if(elements.classList.contains('pausar')) {
+            clearInterval(timer)
+        relogio.classList.add('pausar')
+        }
+        if(elements.classList.contains('parar')) {
+            clearInterval(timer)
+            relogio.innerHTML = '00:00:00'
+            seconds = 0;
+        }
+    })
+
+}
+
+relogio()
